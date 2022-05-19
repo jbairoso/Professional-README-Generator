5337552bbb5f0a22a8fe69f595623fd001bfba34
jbairoso <jbairoso@gmail.com>
2022-05-18 17:57:40 -0700

    add more to user info

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const generateReadMe= require("./src/page-template");
+const fs = require("fs");
+
+const generateMarkdown = require("./src/page-template");
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () => {
+  return inquirer.prompt([
     {
-        type:"input",
-        name:"name",
-        message:"What is your name? (Required)",
-        validate: (nameInput) =>{
-            if (nameInput){
-                return true;
-            }else{
-                console.log("Please enter your Name!");
-                return false;
-            }
-        },
+      type: "input",
+      name: "name",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your Name!");
+          return false;
+        }
+      },
     },
     {
-        type:"input",
-        name:"github",
-        message:"Enter your GitHub Username",
-        validate: (nameInput) => {
-            if (nameInput){
-                return true;
-            }else{
-                console.log("Please enter your GitHub Username!");
-            }
-        },
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub Username!");
+        }
+      },
     },
-    
-];
+    {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+      when: ({ confirmAbout }) => confirmAbout,
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
