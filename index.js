5a04b3b1768ba0617d87fb6a822657c04388ddf4
jbairoso <jbairoso@gmail.com>
2022-05-18 20:01:52 -0700

    add licenses

--- index.js
+++ index.js
@@ -6,53 +6,140 @@ const generateMarkdown = require("./src/page-template");
 
 // TODO: Create an array of questions for user input
 const questions = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name? (Required)",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter your Name!");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your Name!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub Username",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter your GitHub Username!");
-        }
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your GitHub Username!");
+          }
+        },
       },
-    },
-    {
-      type: "confirm",
-      name: "confirmAbout",
-      message:
-        'Would you like to enter some information about yourself for an "About" section?',
-      default: true,
-    },
-    {
-      type: "input",
-      name: "about",
-      message: "Provide some information about yourself:",
-      when: ({ confirmAbout }) => confirmAbout,
-    },
-  ]);
+      {
+        type: "input",
+        name: "Title",
+        message: "What is the name of your project?",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your GitHub Username!");
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Description",
+        message: "Provide some information about your project",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Provide information about your project");
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project? Provide a step by step process",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use. Include screenshots as needed.",
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "What did you build this project with? (Check all that apply)",
+        choices: [
+          "JavaScript",
+          "HTML",
+          "CSS",
+          "ES6",
+          "jQuery",
+          "Bootstrap",
+          "Node",
+        ],
+      },
+      {
+        type: "input",
+        name: "credit",
+        message: "Who helped make this awesome project?",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please list all collaborators");
+          }
+        },
+      },
+      {
+        type: "confirm",
+        name: "test",
+        message: "Would you like to go the extra mile and write tests for your application? Provide examples on how to run them here",
+      },
+      {
+        type: "checkbox",
+        name: "licenses",
+        message: "Choose a License",
+        choices: [
+          "None",
+          "Apache License 2.0",
+          "GNU General Public License v3.0",
+          "MIT License",
+          "BSD 2-Clause 'Simplified' License",
+          "BSD 3-Clause 'New' or 'Revised' License",
+          "Boost Software License 1.0",
+          "Creative Commons Zero v1.0 Universal",
+          "Eclipse Public License 2.0",
+          "GNU Affero General Public License v3.0",
+          "GNU General Public License v2.0",
+          "GNU Lesser General Public License v2.1",
+          "Mozilla Public License 2.0",
+          "The Unlicense",
+        ],
+      },
+     
+    ])
+    .then((userResponse) => {
+      console.log(userResponse);
+    });
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   
+}
 
 // Function call to initialize app
-init();
+questions();
