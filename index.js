37920f096bed4d4ad74aa378f738249644173647
jbairoso <jbairoso@gmail.com>
2022-05-18 17:41:27 -0700

    user info

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateReadMe= require("./src/page-template");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name:"name",
+        message:"What is your name? (Required)",
+        validate: (nameInput) =>{
+            if (nameInput){
+                return true;
+            }else{
+                console.log("Please enter your Name!");
+                return false;
+            }
+        },
+    },
+    {
+        type:"input",
+        name:"github",
+        message:"Enter your GitHub Username",
+        validate: (nameInput) => {
+            if (nameInput){
+                return true;
+            }else{
+                console.log("Please enter your GitHub Username!");
+            }
+        },
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
